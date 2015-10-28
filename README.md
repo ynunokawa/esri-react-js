0f0e40f6838ad047f4d29e4a2ad194f14eb76070
ynunokawa <yusuke_nunokawa@esrij.com>
2015-10-28 16:40:36 +0900

    add gif

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [Esri JS](https://developers.arcgis.com/javascript/) apps with [React](https://facebook.github.io/react/index.html) for sample.
 
 * [esri-fixed-data-table (JSX)](https://ynunokawa.github.io/esri-react-js/esri-fixed-data-table/)
+
+<img src="demo-EFDT.gif" style="width:300px;"/>
--- esri-fixed-data-table/src/config.js
+++ esri-fixed-data-table/src/config.js
@@ -10,17 +10,14 @@
     packages: [{
       name: 'react',
       location: 'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/',
-      //location: locationPath + '/lib/react-0.14.0/build',
       main: 'react'
     }, {
       name: 'reactDom',
       location: 'https://cdnjs.cloudflare.com/ajax/libs/react/0.14.0/',
-      //location: locationPath + '/lib/react-0.14.0/build',
       main: 'react-dom'
     }, {
       name: 'fixedDataTable',
       location: 'https://cdnjs.cloudflare.com/ajax/libs/fixed-data-table/0.4.7/',
-      //location: locationPath + '/fixed-data-table/dist',
       main: 'fixed-data-table'
     }]
   };
